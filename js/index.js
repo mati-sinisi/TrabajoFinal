02eb770e3964dfca0be13941031f8c8e1dd8f54d
fckland <matt.fckland@gmail.com>
2022-09-25 22:47:26 -0300

    Modif js

--- js/index.js
+++ js/index.js
@@ -36,12 +36,10 @@ const menuIcon = document.querySelector(".menuIcon");
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
     menu.classList.remove("showMenu");
-    closeIcon.style.display = "none";
     menuIcon.style.display = "block";
   } else {
     menu.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
+    menuIcon.style.display = "block";
   }
 }
 
